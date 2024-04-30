763c79926709a142474c6a0ecd7ce75a655ca8d4
Ryan-Thienpont <Ryan.thienpont@student.howest.be>
2024-04-30 14:16:52 +0200

    Add project entity

--- TeamTrackPro/TeamTrackPro.Core/Entities/Project.cs
+++ TeamTrackPro/TeamTrackPro.Core/Entities/Project.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace TeamTrackPro.Core.Entities
 {
-    public class User
+    public class Project
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CreatedByUserId { get; set; }
     }
 }
